f9ce1e8243f9fa891a2987f3ba5f0449cc5305bc
MatthewRasa <matt@raztech.com>
2017-05-09 22:09:11 -0400

    Added support for Arch Linux in setup.py

--- setup.sh
+++ setup.sh
@@ -1,46 +1,68 @@
 #!/bin/sh
 ############################################
-echo "Welcome to PiCAST 3! \n\n\n"
 
-echo "Performing Aptitude Update... \n"
-sudo apt-get update
+# Determine Linux distribution
+. /etc/os-release
+case "$NAME" in
+	"Arch Linux"|"Arch Linux ARM")
+		packages="git gcc make python python-pip nodejs npm youtube-dl lame mpg123 mplayer streamlink x264 ffmpeg"
+		upgradecmd="pacman -Syu --noconfirm"
+		installcmd="pacman -S --noconfirm"
+		;;
+	"Raspbian GNU/Linux")
+		packages="python-dev python-pip nodejs npm youtube-dl lame mpg321 mplayer livestreamer git build-essential"
+		upgradecmd="apt-get -y upgrade && apt-get -y upgrade"
+		installcmd="apt-get -y install"
+		;;
+esac
 
-echo "Performing any upgrades (just in case)... \n"
-sudo apt-get upgrade
+echo -e "Welcome to PiCAST 3! \n\n\n"
 
-echo "Ok, lets get to the requirements, bare with me... \n"
-sudo apt-get install python-dev python-pip nodejs npm youtube-dl lame mpg321 mplayer livestreamer git build-essential -y
+# Prompt for root credentials if not executed with sudo
+[ $(id -u) -eq 0 ] || sudo -vp "Please enter root password: " || exit 1
+
+echo -e "Performing any package manager upgrades (just in case)... \n"
+sudo $upgradecmd
+
+echo -e "Ok, lets get to the requirements, bare with me... \n"
+sudo $installcmd $packages
 
 echo "I'm too lazy to check if all went well, so lets move on..."
-sleep 2
-clear # wipe everything away...
-echo "I'm going to install H264 Support now, this WILL take some time!!!"
-sleep 3
 
-# We need to get H264 Support & FFMPEG on the system... Repo won't have it...
+# Install source dependencies
+case "$NAME" in
+	"Arch Linux"|"Arch Linux ARM") ;;
+	"Raspbian GNU/Linux")
+		echo "I'm going to install H264 Support now, this WILL take some time!!!"
+		sleep 3
 
-# H264 Process...
-cd /usr/src
-sudo git clone git://git.videolan.org/x264
-cd x264
-sudo ./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl
-sudo make
-sudo make install
+		# We need to get H264 Support & FFMPEG on the system... Repo won't have it...
 
-echo "\n \n I am now going to grab a copy of FFMPEG for MP3 & other reasons..."
-sleep 1
-echo "\n Understand, this would be a good time for coffee or tea... Going to be awhile!"
-sleep 2
+		# H264 Process...
+		cd /usr/src
+		sudo git clone git://git.videolan.org/x264
+		cd x264
+		sudo ./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl
+		sudo make
+		sudo make install
 
-# Process for FFMPEG...
-cd /usr/src # We could have done cd.. but we're taking NO CHANCES...
-sudo git clone git://source.ffmpeg.org/ffmpeg.git
-cd ffmpeg
-sudo ./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree
-sudo make
-sudo make install
+		echo "\n \n I am now going to grab a copy of FFMPEG for MP3 & other reasons..."
+		sleep 1
+		echo "\n Understand, this would be a good time for coffee or tea... Going to be awhile!"
+		sleep 2
+
+		# Process for FFMPEG...
+		cd /usr/src # We could have done cd.. but we're taking NO CHANCES...
+		sudo git clone git://source.ffmpeg.org/ffmpeg.git
+		cd ffmpeg
+		sudo ./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree
+		sudo make
+		sudo make install
+		;;
+esac
 
 # DOWNLOAD/INSTALL FOREVER TO KEEP PICAST RUNNING FOREVER... HAHA?
+
 sudo npm install forever -g
 sudo npm install forever-monitor -g
 
@@ -50,6 +72,9 @@ echo "Making PiCAST Folder..."
 mkdir PiCAST
 echo "Entering PiCAST Folder..."
 cd PiCAST
+
+# Install local node dependencies
+sudo npm install express
 
 echo "Getting PiCAST Server file..."
 sleep 1
